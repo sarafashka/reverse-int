5726dd61815f382bbcaaae40699bee3ac2195864
Natalya Stepanova <natalyastepanova@MacBook-Air-Natalya.local>
2022-01-08 18:06:06 +0300

    feat: make a task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let str = String(Math.abs(n));
+    for (i = 0; i < str.length; i ++) {
+        result = `${str[i]}${result}`;
+    } return Number(result);
 }
